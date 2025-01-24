57c910115a73738f9cc34a0b571eeb94d0d423f0
Dmitriy Kolesnik <13na18@gmail.com>
2025-01-24 21:24:12 +0200

    feat: Add CurrencyEntity, ExchangeRateEntity

---
+++ src/main/java/org/proj3/currencyexchange/entity/CurrencyEntity.java
@@ -0,0 +1,57 @@
+package org.proj3.currencyexchange.entity;
+
+public class CurrencyEntity {
+    private final int id;
+    private String code;
+    private String fullName;
+    private String sign;
+
+    public CurrencyEntity(int id, String code, String fullName, String sign) {
+        this.id = id;
+        this.code = code;
+        this.fullName = fullName;
+        this.sign = sign;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public void setSign(String sign) {
+        this.sign = sign;
+    }
+
+    @Override
+    public String toString() {
+        return "CurrencyEntity{" +
+               "id=" + id +
+               ", code='" + code + '\'' +
+               ", fullName='" + fullName + '\'' +
+               ", sign='" + sign + '\'' +
+               '}';
+    }
+}
---
+++ src/main/java/org/proj3/currencyexchange/entity/ExchangeRateEntity.java
@@ -0,0 +1,59 @@
+package org.proj3.currencyexchange.entity;
+
+import java.math.BigDecimal;
+
+public class ExchangeRateEntity {
+    private final int id;
+    private int baseCurrencyId;
+    private int targetCurrencyId;
+    private BigDecimal rate;
+
+    public ExchangeRateEntity(int id, int baseCurrencyId, int targetCurrencyId, BigDecimal rate) {
+        this.id = id;
+        this.baseCurrencyId = baseCurrencyId;
+        this.targetCurrencyId = targetCurrencyId;
+        this.rate = rate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+
+    public int getBaseCurrencyId() {
+        return baseCurrencyId;
+    }
+
+    public void setBaseCurrencyId(int baseCurrencyId) {
+        this.baseCurrencyId = baseCurrencyId;
+    }
+
+    public int getTargetCurrencyId() {
+        return targetCurrencyId;
+    }
+
+    public void setTargetCurrencyId(int targetCurrencyId) {
+        this.targetCurrencyId = targetCurrencyId;
+    }
+
+    public BigDecimal getRate() {
+        return rate;
+    }
+
+    public void setRate(BigDecimal rate) {
+        this.rate = rate;
+    }
+
+    @Override
+    public String toString() {
+        return "ExchangeRateEntity{" +
+               "id=" + id +
+               ", baseCurrencyId=" + baseCurrencyId +
+               ", targetCurrencyId=" + targetCurrencyId +
+               ", rate=" + rate +
+               '}';
+    }
+}
