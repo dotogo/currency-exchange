dcf59ef2f7fdd59aee50e02aa87f14ee9212bf9f
Dmitriy Kolesnik <13na18@gmail.com>
2025-01-26 18:45:09 +0200

    feat: Add CurrencyService.getCurrencies()

--- src/main/java/org/proj3/currencyexchange/service/CurrencyService.java
+++ src/main/java/org/proj3/currencyexchange/service/CurrencyService.java
@@ -5,11 +5,30 @@ import org.proj3.currencyexchange.dto.CurrencyDto;
 import org.proj3.currencyexchange.entity.CurrencyEntity;
 import org.proj3.currencyexchange.mapper.CurrencyMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CurrencyService {
     private static final CurrencyService currencyService = new CurrencyService();
     private static final CurrencyDao currencyDao = CurrencyDao.getInstance();
 
     private CurrencyService() {
+    }
+
+    public List<CurrencyDto> getCurrencies() {
+        List<CurrencyEntity> entities = currencyDao.getCurrencies();
+//        List<CurrencyDto> dtos = new ArrayList<CurrencyDto>();
+        CurrencyMapper mapper = CurrencyMapper.getInstance();
+        return entities.stream()
+                .map(mapper::toDto)
+                .toList();
+
+//        for (CurrencyEntity entity : entities) {
+//            CurrencyMapper mapper = CurrencyMapper.getInstance();
+//            CurrencyDto dto = mapper.toDto(entity);
+//            dtos.add(dto);
+//        }
+
     }
 
     public CurrencyEntity save(CurrencyDto currencyDto) {
